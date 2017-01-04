151c61ad671bdc83f02018e5f8957235fa009a43
Pachnik Tomas <tomas.pachnik@anext.sk>
2017-01-04 10:52:23 +0100

    konfiguracia presunuta do xml

--- src/main/java/sk/tomas/app/configuration/Application.java
+++ src/main/java/sk/tomas/app/configuration/Application.java
@@ -1,12 +1,11 @@
 package sk.tomas.app.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by tomas on 23.12.2016.
@@ -14,13 +13,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @SpringBootApplication
 @ComponentScan("sk.tomas.app")
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+@ImportResource("classpath:application-context.xml")
 public class Application {
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class);
     }
 
 }
