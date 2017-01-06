fe1bd8c1f1cf76b9ad47d40ecf156f6a6517dfca
Tomáš Páchnik <tomas.pachnik@gmail.com>
2017-01-06 15:02:39 +0100

    opravena zla konfiguracia testov sposobena pridanim anotacie EnableWebMvc

--- src/test/java/sk/tomas/app/tests/BaseTest.java
+++ src/test/java/sk/tomas/app/tests/BaseTest.java
@@ -3,12 +3,14 @@ package sk.tomas.app.tests;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Created by Tomas Pachnik on 04-Jan-17.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration("classpath:application-context-test.xml")
 public abstract class BaseTest {
 }
