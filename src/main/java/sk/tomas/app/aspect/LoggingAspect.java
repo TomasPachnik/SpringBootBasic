ae46006c24acefd6b3501f49c1cf537dc4485cf8
Tomáš Páchnik <tomas.pachnik@gmail.com>
2017-01-06 11:15:38 +0100

    presunuta konfiguracia aspektu z xml do anotacie

--- src/main/java/sk/tomas/app/aspect/LoggingAspect.java
+++ src/main/java/sk/tomas/app/aspect/LoggingAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
@@ -12,7 +13,7 @@ import java.util.Arrays;
  * Created by tomas on 06.01.2017.
  */
 
-
+@Component
 @Aspect
 public class LoggingAspect {
 
