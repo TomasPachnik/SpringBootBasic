522e70ed906c9100609266eda0d3647a8fe26ca4
Pachnik Tomas <tomas.pachnik@anext.sk>
2017-01-12 14:01:24 +0100

    pridany chybajuci super

--- src/main/java/sk/tomas/app/exception/BusinessException.java
+++ src/main/java/sk/tomas/app/exception/BusinessException.java
@@ -3,14 +3,9 @@ package sk.tomas.app.exception;
 /**
  * Created by Tomas Pachnik on 12-Jan-17.
  */
-public class BusinessException extends Exception {
+class BusinessException extends Exception {
 
-    public BusinessException(String message) {
+    BusinessException(String message) {
         super(message);
-    }
-
-    public BusinessException(String message, Throwable cause) {
-
-        super(message, cause);
     }
 }
