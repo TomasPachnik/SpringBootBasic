7f7246bf7d7088e903441523f7e90c7c5f4f98c0
Pachnik Tomas <tomas.pachnik@anext.sk>
2017-03-02 13:11:04 +0100

    do testu pridane mapovanie input/output objektov

--- src/test/java/sk/tomas/app/tests/MapperTest.java
+++ src/test/java/sk/tomas/app/tests/MapperTest.java
@@ -4,6 +4,10 @@ import ma.glasnost.orika.MapperFacade;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import sk.tomas.app.iam.model.input.IdentityInput;
+import sk.tomas.app.iam.model.input.RoleInput;
+import sk.tomas.app.iam.model.output.IdentityOutput;
+import sk.tomas.app.iam.model.output.RoleOutput;
 import sk.tomas.app.model.Identity;
 import sk.tomas.app.model.Role;
 import sk.tomas.app.orm.IdentityNode;
@@ -22,20 +26,26 @@ public class MapperTest extends BaseTest {
 
     @Test
     public void identityMapperTest() {
-        Identity identity = Utils.createRandomIdentity();
+        IdentityInput identityInput = Utils.randomIdentity();
+        Identity identity = mapper.map(identityInput, Identity.class);
         identity.setUuid(UUID.randomUUID());
         IdentityNode identityNode = mapper.map(identity, IdentityNode.class);
         Identity mapped = mapper.map(identityNode, Identity.class);
-        Assert.assertTrue("Identity sa nezhoduju", identity.equals(mapped));
+        IdentityOutput identityOutput = mapper.map(mapped, IdentityOutput.class);
+        IdentityInput identityInput1 = mapper.map(identityOutput, IdentityInput.class);
+        Assert.assertTrue("Identity sa nezhoduju", identityInput.equals(identityInput1));
     }
 
     @Test
     public void roleMapperTest() {
-        Role role = Utils.createRandomRole();
+        RoleInput roleInput = Utils.randomRole();
+        Role role = mapper.map(roleInput, Role.class);
         role.setUuid(UUID.randomUUID());
         RoleNode roleNode = mapper.map(role, RoleNode.class);
         Role mapped = mapper.map(roleNode, Role.class);
-        Assert.assertTrue("Roly sa nezhoduju", role.equals(mapped));
+        RoleOutput roleOutput = mapper.map(mapped, RoleOutput.class);
+        RoleInput roleInput1 = mapper.map(roleOutput, RoleInput.class);
+        Assert.assertTrue("Roly sa nezhoduju", roleInput.equals(roleInput1));
     }
 
 }
